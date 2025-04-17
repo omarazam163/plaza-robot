0329460ec4e36d8473b191905aa8284511b568ae
omar ahmed <50985259+omarazam163@users.noreply.github.com>
2025-04-17 19:48:09 +0200

    added movement with r2 and l2

--- src/main.cpp
+++ src/main.cpp
@@ -52,19 +52,38 @@ void loop() {
 
 
 void carMovement() {
-  if (Ps3.data.analog.stick.ly<=-25)
+  // if (Ps3.data.analog.stick.ly<=-25)
+  // {
+  //     int speed = map(abs(Ps3.data.analog.stick.ly),25,128,0,255);
+  //     int speeds[2] = {speed, speed}; // 0 index => left 1 index => right
+  //     carMovementX(speeds);
+  //     Movement(speeds[0], speeds[1],direction::forward);
+  // }
+  // else if (Ps3.data.analog.stick.ly>=25)
+  // {
+  //     int speed = map(abs(Ps3.data.analog.stick.ly),25,128,0,255);
+  //     int speeds[2] = {speed, speed}; // 0 index => left 1 index => right
+  //     carMovementX(speeds);
+  //     Movement(speeds[0], speeds[1],direction::reverse);
+  // }
+  // else
+  // {
+  //   Movement(0, 0,direction::stop);
+  // }
+  // Serial.println(Ps3.data.analog.button.l2);
+  if(Ps3.data.analog.button.r2>=25)
   {
-      int speed = map(abs(Ps3.data.analog.stick.ly),25,128,0,255);
-      int speeds[2] = {speed, speed}; // 0 index => left 1 index => right
-      carMovementX(speeds);
-      Movement(speeds[0], speeds[1],direction::forward);
+    int speed = map(abs(Ps3.data.analog.button.r2),25,150,0,255);
+    int speeds[2] = {speed, speed}; // 0 index => left 1 index => right
+    carMovementX(speeds);
+    Movement(speeds[0], speeds[1],direction::forward);
   }
-  else if (Ps3.data.analog.stick.ly>=25)
+  else if (Ps3.data.analog.button.l2>=25)
   {
-      int speed = map(abs(Ps3.data.analog.stick.ly),25,128,0,255);
-      int speeds[2] = {speed, speed}; // 0 index => left 1 index => right
-      carMovementX(speeds);
-      Movement(speeds[0], speeds[1],direction::reverse);
+    int speed = map(abs(Ps3.data.analog.button.l2),25,150,0,255);
+    int speeds[2] = {speed, speed}; // 0 index => left 1 index => right
+    carMovementX(speeds);
+    Movement(speeds[0], speeds[1],direction::reverse);
   }
   else
   {
@@ -73,19 +92,35 @@ void carMovement() {
 }
 
 void carMovementX(int speeds[]) {
+  Serial.print("normal ");
+  Serial.print(speeds[0]);
+  Serial.print(" ");  
+  Serial.println(speeds[1]);
   if (Ps3.data.analog.stick.lx<=-25)
   {
       int speedMinus = map(abs(Ps3.data.analog.stick.lx),25,128,0,255);
       speeds[0] -= speedMinus;
+  Serial.print("left ");
+  Serial.print(speeds[0]);
+  Serial.print(" ");  
+  Serial.println(speeds[1]);
   }
   else if (Ps3.data.analog.stick.lx>=25)
   {
       int speedMinus = map(abs(Ps3.data.analog.stick.lx),25,128,0,255);
       speeds[1] -= speedMinus;
+  Serial.print("right ");
+  Serial.print(speeds[0]);
+  Serial.print(" ");  
+  Serial.println(speeds[1]);
   }
 }
 
 void Movement(int speedLeft, int speedRight, direction dir) {
+  if(speedLeft > 255) speedLeft = 255;
+  if(speedRight > 255) speedRight = 255;
+  if(speedLeft < 0) speedLeft = 0;
+  if(speedRight < 0) speedRight = 0;
   if(dir == direction::forward) // forward
   {
     digitalWrite(IN1, HIGH);
@@ -109,7 +144,7 @@ void Movement(int speedLeft, int speedRight, direction dir) {
   }
   analogWrite(ENA, speedLeft);
   analogWrite(ENB, speedRight);
-  delay(10);
+  // delay(1);
 }
 
 
